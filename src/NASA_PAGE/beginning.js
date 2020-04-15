3cc028a0cd8c8c152f62ee9cef54dacacb5c7dda
Shantel Brown <Shantel.Brown2021@gmail.com>
2020-04-15 13:09:43 -0700

    Initial Commit

---
+++ src/NASA_PAGE/beginning.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import "../App";
+import axios from "axios";
+
+function Beginning() {
+  return (
+    <div className="Top">
+      console.log(Beginning)
+    </div>
+  );
+}
+
+export default Beginning;
